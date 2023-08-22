6b76a6c40dfc12c5d903d5ddf78adf3ac89e4c4c
yhj0122 <yhj0122@midasit.com>
2023-08-22 20:07:00 +0900

    first commit

---
+++ FM_BE/hawaiian_pizza/src/test/kotlin/com/pineapple/hawaiian_pizza/HawaiianPizzaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pineapple.hawaiian_pizza
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HawaiianPizzaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
