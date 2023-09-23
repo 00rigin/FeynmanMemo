a4e632ca31e2133e60f2e3a2f756168fabb5d1fd
00rigin <whatacg@gmail.com>
2023-09-23 16:44:52 +0900

    project fix

---
+++ FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/HawaiianPizzaApplication.kt
@@ -0,0 +1,11 @@
+package com.pineapple.hawaiianPizza
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HawaiianPizzaApplication
+
+fun main(args: Array<String>) {
+	runApplication<HawaiianPizzaApplication>(*args)
+}
