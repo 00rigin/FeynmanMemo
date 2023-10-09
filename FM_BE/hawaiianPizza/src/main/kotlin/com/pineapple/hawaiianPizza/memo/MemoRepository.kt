6173e5d88aa2a874f60d6e1497da32fd36f88d1f
00rigin <whatacg@gmail.com>
2023-10-09 18:39:49 +0900

    메모 조회 API

--- FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/memo/MemoRepository.kt
+++ FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/memo/MemoRepository.kt
@@ -1,11 +1,9 @@
 package com.pineapple.hawaiianPizza.memo
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface MemoRepository: JpaRepository<Memo, Long> {
-
+    fun findByTitle(title: String): Memo?
 }
--- FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/memo/MemoService.kt
+++ FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/memo/MemoService.kt
@@ -2,6 +2,7 @@ package com.pineapple.hawaiianPizza.memo
 
 import com.pineapple.hawaiianPizza.memo.dto.MemoDto
 import com.pineapple.hawaiianPizza.memo.dto.MemoRequest
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -9,10 +10,16 @@ import org.springframework.transaction.annotation.Transactional
 class MemoService(private val memoRepository: MemoRepository) {
 
     @Transactional
-    fun postTestMemo(rq: MemoRequest.Create): MemoDto{
-        val parentMemo = memoRepository.findById(rq.parentSn).orElse(null)
+    fun postMemo(rq: MemoRequest.Create): MemoDto{
+        val parentMemo = memoRepository.findByIdOrNull(rq.parentSn)
         val newMemo = memoRepository.save(Memo(rq.title, rq.description, parentMemo))
 
         return MemoDto(newMemo)
+    }
+
+
+    fun getMemo(memoSn: Long): MemoDto{
+        val targetMemo = memoRepository.findByIdOrNull(memoSn) ?: throw IllegalStateException("Memo not found")
+        return MemoDto(targetMemo)
     }
 }
