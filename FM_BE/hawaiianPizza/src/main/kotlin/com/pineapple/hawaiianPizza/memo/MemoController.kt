7dad6eb94a3b9699b55c64ceb98d54e422b34a3f
00rigin <whatacg@gmail.com>
2023-10-09 18:39:49 +0900

    메모 조회 API

--- FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/memo/MemoController.kt
+++ FM_BE/hawaiianPizza/src/main/kotlin/com/pineapple/hawaiianPizza/memo/MemoController.kt
@@ -1,8 +1,10 @@
-package com.pineapple.hawaiianPizza.memo;
+package com.pineapple.hawaiianPizza.memo
 
 import com.pineapple.hawaiianPizza.memo.dto.MemoRequest
 import com.pineapple.hawaiianPizza.memo.dto.MemoResponse
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,7 +15,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/memo")
 class MemoController(private val memoService: MemoService) {
     @PostMapping
-    fun postTestMemo(@RequestBody rq: MemoRequest.Create): MemoResponse.Result{
-        return MemoResponse.Result(memoService.postTestMemo(rq))
+    fun postMemo(@RequestBody rq: MemoRequest.Create): MemoResponse.Result{
+        return MemoResponse.Result(memoService.postMemo(rq))
+    }
+
+    @GetMapping("/{memoSn}")
+    fun getMemo(@PathVariable memoSn: Long): MemoResponse.Result{
+        return MemoResponse.Result(memoService.getMemo(memoSn))
     }
 }
